a8c43e08153d5cde7be86f2e30fe5f604782e0a4
akshara12code <akshara12082005@gmail.com>
2026-01-16 15:39:46 +0530

    Fix data ingestion path and add model training with hyperparameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path:str=os.path.join('artifacts', 'train.csv')
@@ -22,7 +24,7 @@ class DataIngestion:
         logging.info("Entered Data Ingestion")
 
         try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            df=pd.read_csv('notebook/data/stud.csv')
             logging.info("Exported read dataset")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
@@ -51,4 +53,7 @@ if __name__ == "__main__":
     # Create config and pass it
     data_transformation_config = DataTransformationConfig()
     data_transformation = DataTransformation(data_transformation_config)
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr,test_arr,_= data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
